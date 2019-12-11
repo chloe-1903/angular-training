fceacb99a57dd44552cd36948e177f4730aadd2c
chloe-1903 <chloe-1903@live.fr>
2019-12-11 16:09:20 +0100

    transclusion : projecting templates with ng-content

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TotalPipe } from './pipes/total.pipe';
 import { StateDirective } from './directives/state.directive';
+import { TableauLightComponent } from './components/tableau-light/tableau-light.component';
+import { TableauDarkComponent } from './components/tableau-dark/tableau-dark.component';
 
 
 
 @NgModule({
-  declarations: [TotalPipe, StateDirective],
-  exports : [TotalPipe, StateDirective],
+  declarations: [TotalPipe, StateDirective, TableauLightComponent, TableauDarkComponent],
+  exports : [TotalPipe, StateDirective, TableauLightComponent, TableauDarkComponent],
   imports: [
     CommonModule
   ]
